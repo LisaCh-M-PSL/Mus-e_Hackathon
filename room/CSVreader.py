16cf6d5bd8adcf4c5a3437e7244bf035672d65bf
Perrine Allain <perrine.allain@etu.minesparis.psl.eu>
2026-06-29 15:13:28 +0200

    read csv

---
+++ room/CSVreader.py
@@ -0,0 +1,18 @@
+import csv
+import json
+
+dico= []
+with open ('data.csv', mode='r') as file:
+    csv_reader = csv.reader(file, delimiter=';')
+    for salle, nom, desc, photo, audio in csv_reader:
+        dico.append({
+            'salle': salle,
+            'nom': nom,
+            'desc': desc,
+            'photo': photo,
+            'audio': audio
+        })
+
+print(dico)
+with open('data.json', 'w') as json_file:
+    json.dump(dico, json_file, indent=4)
