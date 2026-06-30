a2ad05bf8d53849b7a1565ddfed5360a56ed1994
Ines Gabille <ines.gabille@etu.minesparis.psl.eu>
2026-06-30 10:26:18 +0200

    transparence

--- Infos/infos.js
+++ Infos/infos.js
@@ -1,12 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log(`body has ${document.body.childElementCount} children`);
   let blurimage = (image) =>{
-    image.style.opacity = "0.9";
+    image.style.opacity = "0.7";
+  }
+  let normalimage = (image) =>{
+    image.style.opacity = "1";
   }
 
-  const images = document.querySelectorAll("img");
+function isMobile() {
+   return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+if (!(isMobile())){
+  const images = document.querySelectorAll("img.blur");
   images.forEach((image)=>{ 
-    image.addEventListener('click', ()=> blurimage(image))
-  }
-)
+    image.addEventListener('mouseover', ()=> blurimage(image))
+  })
+  images.forEach((image)=>{ 
+    image.addEventListener('mouseout', ()=> normalimage(image))
+  })
+}
+
+if (isMobile()){
+  const images = document.querySelectorAll("img.blur");
+  images.forEach((image)=>{ 
+    image.addEventListener('touchstart', ()=> blurimage(image))
+  })
+  images.forEach((image)=>{ 
+    image.addEventListener('touchend', ()=> normalimage(image))
+  })
+}
+
 });
